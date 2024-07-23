4851a272a1f263ecb04ff8344db4ca105e19cc39
User <User@DESKTOP-K2LP823>
2024-07-23 08:34:44 +0530

    Add project files.

---
+++ BulkyWeCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyWeCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
